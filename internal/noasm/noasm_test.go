3921ae49d361feab1dc682f5bf603b2ba36dc2eb
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-08-17 15:57:23 +0300

    update file header

--- internal/noasm/noasm_test.go
+++ internal/noasm/noasm_test.go
@@ -1,3 +1,11 @@
+//
+// Copyright (C) 2024 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/kshard/vector
+//
+
 package noasm_test
 
 import (
